4da5e01b919fbc1ba959d4744a3813f545ccf6a2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-09-15 13:43:21 -0300

    variables

---
+++ 03-variables/variables.py
@@ -0,0 +1,41 @@
+a = 15
+b = 20
+c = a + b
+
+print(c)
+
+nombre = "Tomás"
+
+print(nombre)
+
+apellido = "Gonzalez"
+apellido = "Cuevas"
+
+print(apellido)
+
+numero = 10
+numero += 5
+numero -= 3
+
+print(numero)
+
+# Concatenar con +
+bienvenido = "Hola " + nombre + " ¿cómo estás?"
+
+print(bienvenido)
+
+# Concatenar con F string
+saludo = f"Muy buenas {nombre}."
+
+print(saludo)
+
+valor_numerico = 4
+cuenta = f"Hay {valor_numerico} platos en la mesa."
+del valor_numerico
+
+print(cuenta)
+
+validar_texto = "Se encuentra algo en este texto."
+
+print("encuentra" in validar_texto) # True
+print("encuentra" not in validar_texto) # False
