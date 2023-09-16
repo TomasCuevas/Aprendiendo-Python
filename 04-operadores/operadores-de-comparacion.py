c5ba157d280d7317bab240339429832fb73f1fad
Tomas Cuevas <memo_mister398@hotmail.com>
2023-09-16 13:05:55 -0300

    operadores-de-comparacion

---
+++ 04-operadores/operadores-de-comparacion.py
@@ -0,0 +1,32 @@
+igual_que = 5 == 6
+
+print(f"5 es igual que 6: {igual_que}")
+
+distinto_de = 5 != 6
+
+print(f"5 es distinto de 6: {distinto_de}")
+
+mayor_que = 5 > 6
+
+print(f"5 es mayor que 6: {mayor_que}")
+
+menor_que = 5 < 6
+
+print(f"5 es menor que 6: {menor_que}")
+
+mayor_o_igual = 5 >= 6
+
+print(f"5 es mayor o igual que 6: {mayor_o_igual}")
+
+menor_o_igual = 5 <= 6
+
+print(f"5 es menor o igual que 6: {menor_o_igual}")
+
+# Calculos combinados
+
+a = 5
+b = 10
+c = 20
+comparacion = a + b < c
+
+print(f"5 + 10 es menor que 20: {comparacion}")
