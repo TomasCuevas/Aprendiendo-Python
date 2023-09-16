8e3685736e842102b9ca49d1bde82acad722fd05
Tomas Cuevas <memo_mister398@hotmail.com>
2023-09-16 14:13:49 -0300

    condicionales

---
+++ 05-condicionales/elif.py
@@ -0,0 +1,8 @@
+ingreso_mensual = 5_000
+
+if ingreso_mensual > 10_000:
+  print("estas bien economicamente en cualquier parte del mundo")
+elif ingreso_mensual > 1_000:
+  print("estas bien economicamente en latinoamerica")
+else:
+  print("sos pobre")
---
+++ 05-condicionales/if-else.py
@@ -0,0 +1,14 @@
+edad = 16
+
+if edad >= 18:
+  print("podes pasar")
+else: 
+  print("no podes pasar")
+
+contraseña_almacenada = "MiContraseña123"
+contraseña_escrita = "MiContraseña123"
+
+if contraseña_almacenada == contraseña_escrita:
+  print("contraseña correcta")
+else: 
+  print("contraseña incorrecta")
