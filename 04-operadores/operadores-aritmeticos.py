6fdbebd5b57d0b0f30097fd5a3aacc9e21935c59
Tomas Cuevas <memo_mister398@hotmail.com>
2023-09-16 12:57:46 -0300

    operadores-aritmeticos

---
+++ 04-operadores/operadores-aritmeticos.py
@@ -0,0 +1,33 @@
+# suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+
+# multiplicación y división (* y /)
+multi = 12 * 5
+division = 12 / 6
+
+print(f"Multi: {multi}")
+print(f"Division: {division}")
+
+# potenciación (exponente) (**)
+exponente = 12**3
+
+print(f"Potencia: {exponente}")
+
+# division baja (//)
+division_baja = 12//5 # Devuelve un entero redondeado hacia abajo
+
+print(f"Division baja: {division_baja}")
+
+# resto o módulo
+resto = 12%5
+
+print(f"Resto: {resto}")
+
+# type(dato) nos devuelve que tipo de dato es
+tipo_de_dato = type(division)
+
+print(tipo_de_dato)
