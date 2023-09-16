f70258fe019493ef63cd72aca339ec3f630be63f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-09-16 14:20:44 -0300

    operadores-logicos

---
+++ 04-operadores/operadores-logicos.py
@@ -0,0 +1,15 @@
+# AND
+resultado1 = True & True # Devuelve True
+resultado2 = False & True # Devuelve False
+resultado3 = True & False # Devuelve False
+resultado4 = False & False # Devuelve False
+
+# OR
+resultado5 = True | True # Devuelve True
+resultado6 = False | True # Devuelve True
+resultado7 = True | False # Devuelve True
+resultado8 = False | False # Devuelve False
+
+# NOT
+resultado9 = not True # Devuelve False
+resultado10 = not False # Devuelve True
