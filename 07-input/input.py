2699953d61799be98f924417d15273f431467d6a
Tomas Cuevas <memo_mister398@hotmail.com>
2023-09-18 15:08:52 -0300

    input

---
+++ 07-input/input.py
@@ -0,0 +1,13 @@
+# Pedirle un dato al usuario
+nombre = input("Inserte su nombre: ")
+print(f"El nombre es: {nombre}")
+
+# Convertir un string a int y lo multiplico por 2 (int)
+numero = input("Inserte un numero: ")
+resultado = int(numero) * 2
+print(resultado)
+
+# Convertir un string a float y lo multiplico por 2 (int)
+numero = input("Inserte un numero: ")
+resultado = float(numero) * 2
+print(resultado)
