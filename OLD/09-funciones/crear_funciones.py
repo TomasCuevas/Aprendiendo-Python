1c0a108c32c810763f3f5668e09c11a36eac13f2
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-17 13:13:36 -0300

    move previous guide files to OLD folder

---
+++ OLD/09-funciones/crear_funciones.py
@@ -0,0 +1,13 @@
+# Crear una funcion simple
+def saludar():
+  print("Muy buenas a todos, guapisimos.")
+
+# Ejecutar la funcion simple
+saludar()
+
+# Crear una funcion que tenga parametros
+def saludar_con_parametro(nombre):
+  print(f"Muy buenas, {nombre}")
+
+saludar_con_parametro("Tomás")
+saludar_con_parametro("Miguel")
