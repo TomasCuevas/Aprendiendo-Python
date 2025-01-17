4e85074ddb4fd4404c8f47bcb328c7cd270ac5d8
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-17 13:28:31 -0300

    Enhance first_project.py with additional print statements demonstrating string manipulation and formatting

--- PYTHON-TOTAL/DAY-1/first_project.py
+++ PYTHON-TOTAL/DAY-1/first_project.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+
+print("Hola" + " " + 'Tomás')
+
+print("Me llamo \"Tomás\"")
+
+print("Esta es una linea\nY esta es otra linea")
+print("\tEsta es la tercera linea")
+
+print('This isn\'t a number' )
+
+print("Este signo \\ es una barra invertida")
