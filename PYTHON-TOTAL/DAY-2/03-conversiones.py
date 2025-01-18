ac37b6fc1da62b23a1a05039ec88023c9ad5a341
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-18 13:02:57 -0300

    Add 03-conversiones.py to demonstrate type conversion, arithmetic operations, and user input handling

---
+++ PYTHON-TOTAL/DAY-2/03-conversiones.py
@@ -0,0 +1,26 @@
+num1 = 20
+num2 = 30.5
+
+num1 = num1 + num2
+
+print(type(num1))
+print(type(num2))
+print("\n")
+
+num3 = 5.8
+print(num3)
+print(type(num3))
+print("\n")
+
+num4 = int(num3)
+print(num4)
+print(type(num4))
+print("\n")
+
+edad = input("Escribe tu edad: ")
+edad = int(edad)
+nueva_edad = edad + 1
+print(edad)
+print(nueva_edad)
+print(type(edad))
+print(type(nueva_edad))
