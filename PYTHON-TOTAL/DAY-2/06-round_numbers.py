da6a221f2fbc1cfc93a02b20516261d041054e77
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-18 14:47:32 -0300

    Add 06-round_numbers.py to demonstrate rounding numbers using the round() function, including examples of rounding a division result and formatting a float to specified decimal places.

---
+++ PYTHON-TOTAL/DAY-2/06-round_numbers.py
@@ -0,0 +1,10 @@
+resultado = 90/7
+redondeo = round(resultado)
+print(redondeo)
+print("\n")
+
+valor = 95.7745354343
+print(round(valor, 2))
+print(round(valor, 4))
+print(type(valor))
+print(type(round(valor)))
