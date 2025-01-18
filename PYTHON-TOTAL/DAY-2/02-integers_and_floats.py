bdda14996ce852417497fe116ae55a5070d3b36a
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-18 12:45:26 -0300

    Add 02-integers_and_floats.py to demonstrate integer and float operations, user input, and type checking

---
+++ PYTHON-TOTAL/DAY-2/02-integers_and_floats.py
@@ -0,0 +1,18 @@
+mi_numero = 1 + 3
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+print("\n")
+
+mi_numero_float = 5.8
+print(mi_numero_float)
+print(type(mi_numero_float))
+print("\n")
+
+mi_numero_float = 1 + 5.8
+print(mi_numero_float)
+print(type(mi_numero_float))
+print("\n")
+
+edad = input('Escribe tu edad: ')
+print("Tu edad es " + edad)
+print(type(edad))
