96738e6be36436fbe7ab63902703e37f0ebd590c
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-18 12:18:09 -0300

    Add 01-variables.py to demonstrate variable usage, user input, and basic arithmetic operations

---
+++ PYTHON-TOTAL/DAY-2/01-variables.py
@@ -0,0 +1,20 @@
+nombre = 'Tomás'
+print(nombre)
+
+nombre = 'Laura'
+print(nombre + '\n')
+
+edad = 24
+print(edad)
+
+n1 = 200
+n2 = 300
+print(n1 + n2)
+
+nombre_del_usuario = input('\nEscribe tu nombre: ')
+print("Tu nombre es " + nombre_del_usuario + '\n')
+
+frase_1 = 'Hola '
+frase_2 = 'Mundo'
+frase_3 = '!'
+print(frase_1 + frase_2 + frase_3)
