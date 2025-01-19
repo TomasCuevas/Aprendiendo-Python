d1c64c82b54f2ad1fd058a1dabb35b07f86d5866
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-19 14:40:26 -0300

    Add 01-index.py to demonstrate string indexing and searching techniques
    - This new file includes examples of accessing characters by positive and negative indices, finding the index of a character and a substring, and using rindex to locate the last occurrence of a character in a string.

---
+++ DAY-3/01- index.py
@@ -0,0 +1,25 @@
+mi_texto = "Esta es una prueba"
+
+resultado = mi_texto[3]
+print(f"El caracter del texto en la posición 3 es '{resultado}'")
+print("\n")
+
+resultado = mi_texto[-4]
+print(f"El caracter del texto en la posición -4 es '{resultado}'")
+print("\n")
+
+resultado = mi_texto.index("n")
+print(f"El caracter del texto en la posición {resultado} es 'n'")
+print("\n")
+
+resultado = mi_texto.index("prueba")
+print(f"La palabra 'prueba' comienza en la posición {resultado}")
+print("\n")
+
+resultado = mi_texto.index("a", 5, 12)
+print(f"La primera letra 'a' comenzando desde la posición 5 se encuentra en la posición {resultado}")
+print("\n")
+
+resultado = mi_texto.rindex("a")
+print(f"La última letra 'a' se encuentra en la posición {resultado}")
+print("\n")
