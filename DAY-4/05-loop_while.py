1b814d74de45b1316eff35ce4015814154c839cd
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-20 16:29:07 -0300

    Add 05-loop_while.py to demonstrate while loops in Python, including usage of break, continue, and pass statements.

---
+++ DAY-4/05-loop_while.py
@@ -0,0 +1,34 @@
+# LOOP WHILE: Si bien los loops while son otro tipo de bucles, resultan más parecidos a los condicionales if que a los loops for. 
+# Podemos pensar a los loops while como una estructura condicional que se ejecuta en repetición, hasta que se convierte en falsa.
+
+# BREAK: Si el código llega a una instrucción break, se produce la salida del bucle.
+# CONTINUE: La instrucción continue interrumpe la iteración actual dentro del bucle, llevando al programa a la parte superior del bucle.
+# PASS: La instrucción pass no altera el programa, ocupa un lugar donde se espera una declaración, pero no se desea realizar una acción. 
+
+monedas = 5 
+
+while monedas > 0:
+  print(f"Hay {monedas} monedas")
+  monedas -= 1
+
+print("\n")
+respuesta = 's'
+while respuesta == 's':
+  respuesta = input("Quieres jugar otra vez? (s/n): ")
+else:
+  print("Gracias!")
+  
+print("\n")
+nombre = input("Ingresa tu nombre: ")
+for letra in nombre:
+  if letra == 'a':
+    break
+  
+  print(f"Letra: {letra}")
+  
+print("\n")
+for letra in nombre:
+  if letra == 'a':
+    continue
+  
+  print(f"Letra: {letra}")
