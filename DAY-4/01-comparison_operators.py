c5596b0d0c1374c2cb20a08b146a025f5bfc8584
Tomas Cuevas <memo_mister398@hotmail.com>
2025-01-20 14:39:35 -0300

    Add 01-comparison_operators.py to introduce comparison operators in Python, demonstrating equality, inequality, and relational comparisons with examples and print statements for clarity.

---
+++ DAY-4/01-comparison_operators.py
@@ -0,0 +1,44 @@
+# OPERADORES DE COMPARACION: Como su nombre lo indica, sirven para comparar dos o másvalores. El resultado de esta comparación es un booleano(True/False)
+
+# IGUAL A ==
+# DIFERENTE A !=
+# MAYOR QUE >
+# MENOR QUE <
+# MAYOR O IGUAL A >=
+# MENOR O IGUAL A <=
+
+mi_booleano = 10 == 25 
+print(f"10 == 25: {mi_booleano}")
+print("\n")
+
+mi_booleano = (5 + 5) == 10
+print(f"(5+5) == 10: {mi_booleano}")
+print("\n")
+
+mi_booleano = "blanco" == "blanco"
+print(f"'blanco' == 'blanco': {mi_booleano}")
+print("\n")
+
+mi_booleano = "blanco" == "Blanco"
+print(f"'blanco' == 'Blanco': {mi_booleano}")
+print("\n")
+
+mi_booleano = "100" == 100
+print(f"'100' == 100: {mi_booleano}")
+print("\n")
+
+mi_booleano = 100 != 100
+print(f"100 != 100: {mi_booleano}")
+print("\n")
+
+mi_booleano = 100 > 65
+print(f"100 > 65: {mi_booleano}")
+print("\n")
+
+mi_booleano = 100 > 65
+print(f"100 > 65: {mi_booleano}")
+print("\n")
+
+mi_booleano = 55 < 24
+print(f"55 < 24: {mi_booleano}")
+print("\n")
