bcc1dc200878bc194ea2c99b3caf22fd561f61bc
Tomas Cuevas <memo_mister398@hotmail.com>
2025-02-01 20:27:20 -0300

    Add examples of Python modules and packages with imports and function definitions

---
+++ DAY-8/01/mi_paquete/ejercicio.py
@@ -0,0 +1,7 @@
+from paquete import suma_y_resta
+from paquete.subpaquete import saludo
+
+suma_y_resta.suma(5, 3)
+suma_y_resta.resta(5, 3)
+
+saludo.saludar()
---
+++ DAY-8/01/mi_paquete/paquete/subpaquete/saludo.py
@@ -0,0 +1,2 @@
+def saludar():
+    print("Hola, estoy en un subpaquete")
