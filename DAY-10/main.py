a902ffb3303b58a0feee49f1b7b2af62957636bc
Tomas Cuevas <tomas.contact.dev@gmail.com>
2025-02-14 03:27:55 -0300

    Add game window setup with Pygame, including screen initialization, title, and logo

--- DAY-10/main.py
+++ DAY-10/main.py
@@ -1,13 +1,24 @@
 import pygame
 
+# Inicializar Pygame
 pygame.init()
 
+# Crear la pantalla
 pantalla = pygame.display.set_mode((800, 600))
 
+# Titulo y logo
+pygame.display.set_caption("Invasión Espacial")
+logo = pygame.image.load("ovni.png")
+pygame.display.set_icon(logo)
+
+# Loop del juego
 se_ejecuta = True
 while se_ejecuta:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
+
+    pantalla.fill((205, 144, 228))
+    pygame.display.update()
 
 pygame.quit()
