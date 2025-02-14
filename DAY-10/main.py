d202bafaacafa1d37c8021d7baffae08b2237fb5
Tomas Cuevas <tomas.contact.dev@gmail.com>
2025-02-14 03:32:32 -0300

    Add player spaceship image and initial rendering in space invasion game

--- DAY-10/main.py
+++ DAY-10/main.py
@@ -11,14 +11,26 @@ pygame.display.set_caption("Invasión Espacial")
 logo = pygame.image.load("ovni.png")
 pygame.display.set_icon(logo)
 
+# Jugador
+img_jugador = pygame.image.load("cohete.png")
+jugador_x = 400 - 32
+jugador_y = 600 - 100
+
+def jugador(x, y):
+    pantalla.blit(img_jugador, (x, y))
+
 # Loop del juego
 se_ejecuta = True
 while se_ejecuta:
+    # RGB
+    pantalla.fill((205, 144, 228))
+    
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
 
-    pantalla.fill((205, 144, 228))
+    jugador(jugador_x, jugador_y)
+
     pygame.display.update()
 
 pygame.quit()
